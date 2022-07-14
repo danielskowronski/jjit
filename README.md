7066888f054139cf9155e0c97f2a4b17e55f1199
Daniel Skowroński <daniel@skowron.ski>
2022-07-14 22:08:52 +0200

    adapt code to current API, rewrite it to be better, support new parameters

--- README.md
+++ README.md
@@ -2,24 +2,52 @@
 
 ## requirements
 * python3
-* properly configured [telegram_send](https://pypi.org/project/telegram-send/#installation)
+* properly configured [telegram_send](https://pypi.org/project/telegram-send/#installation); e.g. at `/etc/telegram-send.conf`
 * some witable file to store processed offers (defaulted to /tmp/jjit.txt)
+* `python3 -m pip install -r requirements.txt`
+
+## categories, currencies, contract types
+
+As justjoin.it is evolving categories, currencies and contract types cannot just be put into enums. They are quite obvious in most cases, but to get all possible values you may run:
+
+```bash
+curl -s https://justjoin.it/api/offers > all.json
+echo "== --category"
+cat all.json | jq | grep '"marker_icon"' |sort | uniq -c | sort -rn
+echo "== --currency"
+cat all.json | jq | grep '"currency"' |sort | uniq -c | sort -rn
+echo "== --contract-type"
+cat all.json | jq | grep '"type"' |sort | uniq -c | sort -rn
+``` 
 
 ## usage
 ```
-usage: jjit.py [-h] -c C [C ...] -l L [L ...] [-r] [-f F]
+usage: jjit.py [-h] --category CATEGORY [CATEGORY ...] [--fully_remote] [--location-country LOCATION_COUNTRY [LOCATION_COUNTRY ...]]
+               [--location-city LOCATION_CITY [LOCATION_CITY ...]] [--salary [SALARY]] [--currency [CURRENCY]] [--contract-type [CONTRACT_TYPE]] [--state-file STATE_FILE]
+               [--verbose] [--dont-send]
 
 jjit.py -- check for newest justjoin.it job offers and push them by telegram_send
 
 optional arguments:
-  -h, --help    show this help message and exit
-  -c C [C ...]  categories (like devops, java, c)
-  -l L [L ...]  locations - cities
-  -r            allow remote locations
-  -s [S]        minimum salary
-  -f F          state file
+  -h, --help            show this help message and exit
+  --category CATEGORY [CATEGORY ...]
+                        categories (like devops, java, c)
+  --fully_remote        fully remote offer (i.e. remote interview + remote workplace
+  --location-country LOCATION_COUNTRY [LOCATION_COUNTRY ...]
+                        locations - countries; format - ISO 3166-1 alfa-2 (e.g. PL)
+  --location-city LOCATION_CITY [LOCATION_CITY ...]
+                        locations - cities
+  --salary [SALARY]     minimum salary (assumed gross/month); default - 10000
+  --currency [CURRENCY]
+                        currency (as in offer) [pln, usd, eur, gbp, chf]; default - pln
+  --contract-type [CONTRACT_TYPE]
+                        contract type [permanent, b2b, mandate_contract, ANY]; default - ANY
+  --state-file STATE_FILE
+                        state file; default - /tmp/jjit.txt
+  --verbose             verbose mode for debugging
+  --dont-send           do not actually send offers via Telegram
 ```
 
-Example: `./jjit.py -l Kraków -c devops other -r -s 15000` (this will get offers in Kraków plus remote ones in categories devops and other with minimum salary of 15k PLN).
+Example: `python3 jjit.py --category devops admin support architecture other security --fully_remote --salary 22000`
 
 You'll probably want to put it in crontab.
--- jjit.py
+++ jjit.py
@@ -1,57 +1,238 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import requests, telegram_send, argparse
-
-parser = argparse.ArgumentParser(description='jjit.py -- check for newest justjoin.it job offers and push them by telegram_send')
-parser.add_argument('-c', nargs='+', help='categories (like devops, java, c)', required=True)
-parser.add_argument('-l', nargs='+', help='locations - cities', required=True)
-parser.add_argument('-s', nargs='?', help='minimum salary')
-parser.add_argument('-r', action='store_true', help='allow remote locations')
-parser.add_argument('-f', help='state file')
-args = parser.parse_args()
-
-if args.f==None:
-	file='/tmp/jjit.txt'
-else:
-	file=args.f
-if args.s==None:
-	minsalary=0
-else:
-	minsalary=int(args.s)
-
-f = open(file,'a+')
-idsProcessed=open(file).read()
-
-r = requests.get('https://justjoin.it/api/offers')
-offers = r.json()
-
-for offer in offers:
-	if offer['id'] in idsProcessed:
-		continue
-
-	categorised=False
-	for category in args.c:
-		if offer['marker_icon']==str(category):
-			categorised=True
-
-	localised=False
-	if args.r and offer['remote']:
-		localised=True
-	for city in args.l:
-		if offer['city']==str(city):
-			localised=True
-
-	if offer['salary_to']==None or offer['salary_to']<minsalary:
-		salary_ok=False
-	else:
-		salary_ok=True
+import argparse
+import telegram_send
+import requests
+import sys
+from pprint import pprint
+API_URL = 'https://justjoin.it/api/offers'
+VERBOSE = True
 
 
-	if categorised and localised and salary_ok:
-		msg=str(offer['company_name']+" - "+offer['title']+" https://justjoin.it/offers/"+offer['id'])
-		print(msg)
-		telegram_send.send(messages=[msg], parse_mode="markdown")
-		f.write(","+offer['id'])
+def check_if_offer_already_checked(offer, offer_ids_processed):
+    if VERBOSE:
+        print(f">> check_if_offer_already_checked: {offer['id']}")
+    return offer['id'] in offer_ids_processed
 
-f.close()
+
+def store_offer_variants(offer, state_file):
+    if VERBOSE:
+        print(f"\n>> store_offer_variants: {offer['id']}")
+
+    if offer['multilocation']:
+        for offer_variant in offer['multilocation']:
+            if offer_variant['slug'] != offer['id']:
+                store_offer_id(offer_variant['slug'], state_file)
+
+
+def store_offer_id(id, state_file):
+    if VERBOSE:
+        print(f">> store_offer: {id}")
+    state_file.write(','+id)
+
+
+def check_if_offer_macthes(offer, args):
+    if VERBOSE:
+        print(f">> check_if_offer_macthes: {offer['id']}")
+
+    matches = {
+        'category': False,
+        'fully_remote': False,
+        'location': False,
+        'remuneration': False,
+    }
+
+    for desired_category in args.category:
+        if offer['marker_icon'] == desired_category:
+            matches['category'] = True
+            break
+
+    if args.fully_remote:
+        matches['fully_remote'] = \
+            offer['workplace_type'] == 'remote' and \
+            offer['remote_interview'] == True
+    else:
+        matches['fully_remote'] = True
+
+    matches_location = {
+        'country': False,
+        'city': False
+    }
+
+    if args.location_country:
+        for desired_country in args.location_country:
+            if offer['location']['country_code'] == desired_country:
+                matches_location['country'] = True
+                break
+    else:
+        matches_location['country'] = True
+
+    if args.location_city:
+        for desired_city in args.location_city:
+            if offer['location']['city'] == desired_city:
+                matches_location['city'] = True
+                break
+    else:
+        matches_location['city'] = True
+
+    matches['location'] = \
+        matches_location['country'] and
+    matches_location['city']
+
+    for offered_employment_type in offer['employment_types']:
+        matches_remuneration = {
+            'contract_type': False,
+            'details_disclosed': False,
+            'currency': False,
+            'amount': False
+        }
+
+        matches_remuneration['details_disclosed'] =  \
+            offered_employment_type['salary'] != None and  \
+            offered_employment_type['salary']['to'] != None
+
+        if not matches_remuneration['details_disclosed']:
+            break  # breaking because other detail fields would be missing
+
+        matches_remuneration['contract_type'] = \
+            args.contract_type in offered_employment_type['type']
+
+        matches_remuneration['currency'] = \
+            offered_employment_type['salary']['currency'] == args.currency
+
+        matches_remuneration['amount'] = int(
+            offered_employment_type['salary']['to']) >= args.salary
+
+        matches['remuneration'] = \
+            matches_remuneration['details_disclosed'] and \
+            matches_remuneration['contract_type'] and \
+            matches_remuneration['currency'] and \
+            matches_remuneration['amount']
+
+        if matches['remuneration']:
+            break
+
+    if VERBOSE:
+        print(matches)
+
+    return \
+        matches['category'] and \
+        matches['fully_remote'] and \
+        matches['location'] and \
+        matches['remuneration']
+
+
+def get_offer_url(offer):
+    return 'https://justjoin.it/offers/'+offer['id']
+
+
+def print_offer(offer):
+    if VERBOSE:
+        print(f">> print_offer: {offer['id']}")
+
+    print('Matched offer: '+get_offer_url(offer))
+
+
+def send_offer(offer):
+    if VERBOSE:
+        print(f">> send_offer: {offer['id']}")
+
+    msg = offer['company_name']+' - '+offer['title']+' '+get_offer_url(offer)
+    telegram_send.send(messages=[msg], parse_mode='markdown')
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='jjit.py -- check for newest justjoin.it job offers' +
+        ' and push them by telegram_send')
+
+    parser.add_argument(
+        '--category', nargs='+', required=True,
+        help='categories (like devops, java, c)')
+    parser.add_argument(
+        '--fully_remote', action='store_true',
+        help='fully remote offer (i.e. remote interview + remote workplace')
+    parser.add_argument(
+        '--location-country', nargs='+',
+        help='locations - countries; format - ISO 3166-1 alfa-2 (e.g. PL)')
+    parser.add_argument(
+        '--location-city', nargs='+',
+        help='locations - cities')
+    parser.add_argument(
+        '--salary', nargs='?', type=int, default=10000,
+        help='minimum salary (assumed gross/month); default - 10000')
+    parser.add_argument(
+        '--currency', nargs='?', default='pln',
+        help='currency (as in offer) [pln, usd, eur, gbp, chf]; default - pln')
+    parser.add_argument(
+        '--contract-type', nargs='?', default='ANY',
+        help='contract type [permanent, b2b, mandate_contract, ANY]; ' +
+        'default - ANY')
+    parser.add_argument(
+        '--state-file', default='/tmp/jjit.txt',
+        help='state file; default - /tmp/jjit.txt')
+    parser.add_argument(
+        '--verbose', action='store_true',
+        help='verbose mode for debugging')
+    parser.add_argument(
+        '--dont-send', action='store_true',
+        help='do not actually send offers via Telegram')
+
+    args = parser.parse_args()
+
+    if args.contract_type == 'ANY':
+        args.contract_type = ''
+
+    global VERBOSE
+    VERBOSE = args.verbose
+
+    if VERBOSE:
+        print(f">> args:")
+        pprint(args)
+
+    state_file = open(args.state_file, 'a+')
+    offer_ids_processed = open(args.state_file).read()
+
+    jjit_api_request = requests.get(API_URL)
+    all_offers = jjit_api_request.json()
+
+    if VERBOSE:
+        print("\n\n>> main/before_multioffer_check")
+
+    if not args.location_city:
+        offers_to_check = []
+        for offer in all_offers:
+            offer_already_checked = check_if_offer_already_checked(
+                offer, offer_ids_processed)
+            if not offer_already_checked:
+                offers_to_check.append(offer)
+                store_offer_variants(offer, state_file)
+
+        offer_ids_processed = open(args.state_file).read()
+    else:
+        offers_to_check = all_offers
+
+    if VERBOSE:
+        print("\n\n>> main/after_multioffer_check")
+
+    for offer in offers_to_check:
+        if VERBOSE:
+            print(f"\n>> main/main: {offer['id']}")
+
+        offer_already_checked = check_if_offer_already_checked(
+            offer, offer_ids_processed)
+        if not offer_already_checked:
+            store_offer_id(offer['id'], state_file)
+
+            offer_matches = check_if_offer_macthes(offer, args)
+            if offer_matches:
+                print_offer(offer)
+                if not args.dont_send:
+                    send_offer(offer)
+
+    state_file.close()
+
+
+if __name__ == '__main__':
+    main()
